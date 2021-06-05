f14627dc13ba1df4aae1b12c8859dcdbdb47c445
mohsen-saki <saki.mohsen@gmail.com>
2021-06-06 00:40:27 +1000

    get_attachment.py added

---
+++ get_attachments.py
@@ -0,0 +1,42 @@
+"""
+To extract attachments embeded in a PDF file
+Usage: $ python get_attachment.py YOURFILE.PDF
+
+Thanks to: https://gist.github.com/kevinl95/29a9e18d474eb6e23372074deff2df38#file-extract_pdf_attachments-py
+"""
+
+import PyPDF2
+from sys import argv
+
+def get_attach(reader):
+    """
+    Retrieves the file attachments of the PDF as a dictionary of file names
+    and the file data as a bytestring.
+    :return: dictionary of filenames and bytestrings
+    """
+
+    catalog = reader.trailer["/Root"]
+    file_names = catalog['/Names']['/EmbeddedFiles']['/Names']
+    attachments = {}
+    for f in file_names:
+        if isinstance(f, str):
+            name = f
+            dataIndex = file_names.index(f) + 1
+            fDict = file_names[dataIndex].getObject()
+            fData = fDict['/EF']['/F'].getData()
+            attachments[name] = fData
+
+    return attachments
+
+
+if len(argv) != 2:
+    print("\nWRONG USAGE!")
+    print("Try: $ python get_attachment.py YOURFILE.PDF\n")
+else:
+    handler = open(argv[1], 'rb')
+    reader = PyPDF2.PdfFileReader(handler)
+    dictionary = get_attach(reader)
+    for fName, fData in dictionary.items():
+        with open(fName, 'wb') as outfile:
+            outfile.write(fData)
+    print("Succesfully extracted :)")
